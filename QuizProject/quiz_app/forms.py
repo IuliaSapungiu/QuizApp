d64a1ff5ec278848f0e97bcc617c9eafb32a5e14
IuliaSapungiu <sapungiuiulia555@gmail.com>
2024-07-26 20:36:55 +0300

    Am sters cod invalid.

--- QuizProject/quiz_app/forms.py
+++ QuizProject/quiz_app/forms.py
@@ -10,22 +10,3 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = models.Question
         fields = ['text', 'op1', 'op2', 'op3', 'answer']
-
-# class addQuestionform(forms.ModelForm):
-#     class Meta:
-#         model=models.Quiz
-#         fields="__all__"
-
-
-
-# class QuizForm(forms.ModelForm):
-#     def __init__(self, quiz, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         for question in quiz.questions.all():
-#             choices = [(answer.id, answer.text) for answer in question.answers.all()]
-#             self.fields[f'question_{question.id}'] = forms.MultipleChoiceField(
-#                 choices=choices,
-#                 widget=forms.CheckboxSelectMultiple,
-#                 label=question.text
-#             )
-
